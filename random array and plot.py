ff1e3e81bfba2bf1acc29612dd8c44a7085a0e2c
leskivl <44172839+leskivl@users.noreply.github.com>
2019-09-16 19:07:58 +0300

    random array,plot, avg,variance
    add new function as mean and variance

--- random array and plot.py
+++ random array and plot.py
@@ -1,5 +1,6 @@
 import random
 import matplotlib.pyplot as plt
+import math
 
 n=100
 
@@ -9,3 +10,20 @@ print('a=',a)
 plt.plot(a,'bo')
 plt.show()
 
+def x_average(a):
+  sum_a=sum(a)
+  x_average=sum_a/n
+  return x_average
+
+avg=x_average(a)
+print('average=',avg)
+
+def disp(a):
+  d=0
+  for  i in range(n):
+    d+=(a[i]-avg)**2
+    return d
+ 
+d=disp(a)
+print('disp=',d)
+    
